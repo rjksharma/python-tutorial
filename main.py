7deef562fc6c51f464f5c70a34db4fbb78f6b66a
rajkumarynot <rajkumar@y-not.com>
2026-04-29 20:53:43 +0530

    29-Apr-26 created a function to add two number

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-l = [1,2,3,21,5,6,21,34,5]
-arr = list(set(l))
-arr.sort()
-print(arr[2])
+# l = [1,2,3,21,5,6,21,34,5]
+# arr = list(set(l))
+# arr.sort()
+# print(arr[2])
 
-l2 = {}
+
+class Cal:
+    
+    def __init__(self, value):
+        self.value = value
+    
+    def add(self):
+        return f"Addition will be : {self.value * 2}"
+    
+cls = Cal(10)
+result = cls.add()
+print(result)
