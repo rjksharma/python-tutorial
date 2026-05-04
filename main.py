40da836d7a6ff195c598b5dc9e0d09f3084a192a
rajkumarynot <rajkumar@y-not.com>
2026-05-04 18:52:27 +0530

    finding prime number

--- main.py
+++ main.py
@@ -362,13 +362,16 @@ list2 = [2,3,4,2]
 # else:
 #     print(num, "is not a prime number")
 
-num = int(input("Enter the nmber:- "))
-if num > 1:
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            print("this is not and prime number")
-            break
-    else:
-        print("this is a prime number")
-else:
-    print("This is not prime number")
+
+
+
+# num = int(input("Enter the number"))
+# if num > 1:
+#     for i in range(2, int(num**0.5) + 1):
+#         if num % i == 0:
+#             print("this is not prime number")
+#             break
+#     else:
+#         print("this is prime number")
+# else:
+#     print("This is not prime number")
